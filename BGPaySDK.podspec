066da502d2a4107ecfa077c65550e6f6b24d8783
ldc <lilo7@sina.com>
2018-11-06 11:31:17 +0800

    1.1.0

--- BGPaySDK.podspec
+++ BGPaySDK.podspec
@@ -8,9 +8,9 @@
 
 Pod::Spec.new do |s|
   s.name             = 'BGPaySDK'
-  s.version          = '1.0.0'
+  s.version          = '1.1.0'
   s.summary          = 'BGPay支付SDK'
-  s.swift_version =  '4.1'
+  s.swift_version    =  '4.2'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -40,6 +40,6 @@ Pod::Spec.new do |s|
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
-  s.dependency 'Alamofire', '4.7.1'
-  s.dependency 'CryptoSwift', '0.9.0'
+  s.dependency 'Alamofire'
+  s.dependency 'CryptoSwift'
 end
--- Example/BGPaySDK/AppDelegate.swift
+++ Example/BGPaySDK/AppDelegate.swift
@@ -14,12 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         return true
     }
 
-     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         if url.host == "BigoClient" {
             //TODO:此处处理回调结果
             print("此处处理回调结果")
